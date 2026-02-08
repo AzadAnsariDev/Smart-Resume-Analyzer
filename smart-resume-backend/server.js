7c21d4370a304e608fd6fc998d67d8306d78137a
Azad Ansari <azadansaridev@gmail.com>
2026-02-09 01:36:33 +0530

    Deployment part

--- smart-resume-backend/server.js
+++ smart-resume-backend/server.js
@@ -6,16 +6,24 @@ import mongoose from "mongoose";
 import authRoutes from "./auth.js";
 import resumeRoutes from "./resume.js";
 
-dotenv.config();
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+dotenv.config();
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.json());
 
+
 // MongoDB
 mongoose
-  .connect("mongodb://localhost:27017/Smart-Resume")
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -26,6 +34,11 @@ resumeRoutes(app);
 app.get("/ping", (req, res) => {
   res.send("SRa backend running 🚀");
 });
+
+app.get("*name", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
